179313229138a57d13c40583d680ac7b962d49cc
artemdolia <dolaartem688@gmail.com>
2021-12-13 13:24:53 +0200

    Page Object task1

---
+++ POMTask1/POMTask/PageObjects/LoginPage.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+using Tests;
+
+namespace PageObjects
+{
+    public class LoginPage : Program
+    {
+        private IWebElement ButtonCreateAccount => driver.FindElement(By.XPath("//*[@id='SubmitCreate']/span"));
+        private IWebElement InputEmail => driver.FindElement(By.XPath("//input[@id='email']"));
+        private IWebElement InputPassword => driver.FindElement(By.XPath("//input[@id='passwd']"));
+        private IWebElement SignInButton => driver.FindElement(By.XPath("//button[@id='SubmitLogin']"));
+        private IWebElement TitleAlreadyRegistered => driver.FindElement(By.XPath("//*[@id='login_form']/h3"));
+        private IWebElement ErrorMessage => driver.FindElement(By.XPath("//*[@id='center_column']/div[1]/ol/li"));
+        private IWebElement ParagraphPlsEnterEmail => driver.FindElement(By.XPath("//*[@id='create-account_form']/div/p"));
+
+        public string GetButtonCreate() => ButtonCreateAccount.Text;
+        public void EnterEmail() => InputEmail.SendKeys("dolaartem688@gmail.com");
+        public void EnterPassword() => InputPassword.SendKeys("pass123");
+        public void ClickSignInButton() => SignInButton.Click();
+        public string GetErrorMessage() => ErrorMessage.Text;
+        public string GetTitleAlreadyRegistered() => TitleAlreadyRegistered.Text;
+        public string GetTitlePleaseEnterEmail() => ParagraphPlsEnterEmail.Text;
+    }
+}
---
+++ POMTask1/POMTask/Tests/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using PageObjects;
+using POMTask.Constants;
+
+namespace Tests
+{
+    public class Program
+    {
+        protected static IWebDriver driver;
+ 
+        [SetUp]
+        public void SetUp()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://automationpractice.com/index.php?controller=authentication&back=my-account");
+        }
+        static void Main(string[] args)
+        {
+        }
+
+        [Test]
+        public void CheckNegativeLogin()
+        {
+            LoginPage login = new LoginPage();
+            login.EnterEmail();
+            login.EnterPassword();
+            login.ClickSignInButton();
+            string actualError = login.GetErrorMessage();
+            string expectedError = "Authentication failed.";
+
+            Assert.AreEqual(expectedError, actualError, $"{expectedError} is not equal to {actualError}");
+        }
+
+        [Test]
+        public void GetButtonCreate()
+        {
+            LoginPage login = new LoginPage();
+            string btnCreate = login.GetButtonCreate();
+            Console.WriteLine(btnCreate);
+
+            Assert.AreEqual(Constant.titleCreate, btnCreate, $"Text {btnCreate} is NOT as expected");
+        }
+
+        [Test]
+        public void GetTitleAlreadyRegistered()
+        {
+            LoginPage login = new LoginPage();
+            string titleAlreadyRegistered = login.GetTitleAlreadyRegistered();
+            Console.WriteLine(titleAlreadyRegistered);
+
+            Assert.AreEqual(Constant.txtAlreadyRegistered, titleAlreadyRegistered, "");
+        }
+
+        [Test]
+        public void GetTitlePleaseEnterEmail()
+        {
+            LoginPage login = new LoginPage();
+            string paragraphPlsEnterEmail = login.GetTitlePleaseEnterEmail();
+            Console.WriteLine(paragraphPlsEnterEmail);
+            
+            Assert.AreEqual(Constant.msgPleaseEnter, paragraphPlsEnterEmail, $"The pagraph {paragraphPlsEnterEmail} is NOT found");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Close();
+        }
+    }
+}
+
+
